44fb1616834f9b45d556d4d0c5101c5a4268ffb8
shinamde <Wsxokn@#14>
2022-10-25 23:14:19 +0530

    commit

--- controllers/user.js
+++ controllers/user.js
@@ -1,17 +1,19 @@
 const User = require("../models/User");
 
 exports.updateUser = async (req, res, next) => {
-  const {user_id, user_data } = req.body;
+  const { user_id, user_data } = req.body;
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.userId,{
-      email:user_data.email,
-      education:user_data.education
-    });
+    // const user = await User.findByIdAndUpdate(req.params.userId, {
+    //   email: user_data.email,
+    //   username: user_data.username,
+    //   education: user_data.education
+    // });
 
-    //const user = await User.findById(user_id);
-    user.name = user_data.name;
-    user.education = user_data.education;
+    const user = await User.findById(req.params.userId)
+    user.email =  user_data.email,
+    user.username =  user_data.username,
+    user.education = user_data.education
     user.save()
     res.status(200).json({
       success: true,
@@ -19,29 +21,29 @@ exports.updateUser = async (req, res, next) => {
     });
   } catch (err) {
     next(err);
-  } 
+  }
 };
 
 
 exports.getUserById = async (req, res, next) => {
 
   try {
-  let user = await User.findById(req.params.userId);
+    let user = await User.findById(req.params.userId);
 
-  if (!user) {
-    return res.status(401).json({
-      success: false,
-      msg: "User not found.",
-    });
-  }
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        msg: "User not found.",
+      });
+    }
 
-  res
-    .status(200)
-    .json({ success: true, data: user, msg: "Success" });
+    res
+      .status(200)
+      .json({ success: true, data: user, msg: "Success" });
 
-  }catch (err) {
+  } catch (err) {
     next(err);
-  } 
+  }
 };
 
 
