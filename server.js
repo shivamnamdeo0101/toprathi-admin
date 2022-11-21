1dd9be9d905ea3080663515f35a3cb195784560e
shinamde <Wsxokn@#14>
2022-11-21 17:38:51 +0530

    api

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(cors({
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname, 'client/build')))
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname + '/build/index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'client/build')))
+// app.get('*', (request, response) => {
+//   response.sendFile(path.join(__dirname + '/build/index.html'));
+// });
 
 
 app.get("/", (req, res, next) => {
