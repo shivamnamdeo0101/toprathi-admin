6a1f43b0ba30dbdc31e3cb5171cced8f2980b36d
shinamde <Wsxokn@#14>
2022-12-28 17:39:11 +0530

    api

--- client/src/utils/endPoint.js
+++ client/src/utils/endPoint.js
@@ -1,1 +1,1 @@
-export const endPoint = 'http://localhost:5000/api/';
+export const endPoint = 'https://toprathi-admin.onrender.com/api/';
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const path = require('path');
 connectDB();
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://toprathi-api.herokuapp.com"]
+  origin: ["http://localhost:3000", "https://toprathi-admin.onrender.com"]
 }));
 app.use(express.json());
 
