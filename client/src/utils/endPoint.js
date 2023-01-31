2de6b7d309cfb1d7a0b8115b433b53663fac6886
shinamde <Wsxokn@#14>
2023-01-31 17:16:03 +0530

    sch-filter

--- client/src/utils/endPoint.js
+++ client/src/utils/endPoint.js
@@ -1,2 +1,2 @@
 // export const endPoint = 'http://localhost:5000/api/';
-export const endPoint = "https://toprathi-admin.onrender.com/api/";
+export const endPoint = "https://toprathi.onrender.com/api/";
